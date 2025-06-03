74c417fad1416b075ca8854eccee5b8c8d1e81bb
patrick-hutchinson <hutchinsonpatrick@icloud.com>
2025-06-03 11:08:04 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export", // Outputs a Single-Page Application (SPA).
+  distDir: "./dist", // Changes the build output directory to `./dist/`.
   images: {
     domains: ["cdn.sanity.io", "image.mux.com"],
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
