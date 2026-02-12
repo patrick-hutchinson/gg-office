0eedf1a860728b72fa769248b6de51bc8168cb62
Patrick Hutchinson <hutchinsonpatrick@icloud.com>
2026-02-12 08:47:32 +0000

    Update Credits.jsx

--- src/app/(page)/work/[slug]/components/Credits.jsx
+++ src/app/(page)/work/[slug]/components/Credits.jsx
@@ -32,7 +32,7 @@ export default function Credits({ project }) {
                   </li>
                 ),
             )}
-          {project.creditsInhouse.additionalCredits.map((credit) => (
+          {project.creditsInhouse?.additionalCredits.map((credit) => (
             <li className={`${styles.credit}`} key={credit._id}>
               {credit.role} <br />
               {credit.people.map((person) => (
@@ -56,11 +56,11 @@ export default function Credits({ project }) {
                   </li>
                 ),
             )}
-          {project.creditsClient.additionalCredits.map((credit) => (
+          {project.creditsClient?.additionalCredits.map((credit) => (
             <li className={`${styles.credit}`} key={credit._id}>
               {credit.role} <br />
-              {credit.people.map((person) => (
-                <div>{person}</div>
+              {credit.people.map((person, i) => (
+                <div key={i}>{person}</div>
               ))}
             </li>
           ))}
